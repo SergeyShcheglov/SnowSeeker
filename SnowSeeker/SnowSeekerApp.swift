88733cd668ecfe34e82962f0551b2d4301bef62c
Sergey Shcheglov <97shcheglov@gmail.com>
2022-03-13 19:05:19 +0600

    First commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Sergey Shcheglov on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
