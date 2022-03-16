5a609cacb0f551755b88e79c6564225bb6327840
Sergey Shcheglov <97shcheglov@gmail.com>
2022-03-16 18:04:43 +0600

    Add view to show size and price of resort

--- SnowSeeker/ResortDetailView.swift
+++ SnowSeeker/ResortDetailView.swift
@@ -7,14 +7,47 @@
 
 import SwiftUI
 
-struct ReportsDetailView: View {
+struct ResortDetailView: View {
+    let resort: Resort
+    
+    var size: String {
+        switch resort.size {
+        case 1:
+            return "Small"
+        case 2:
+            return "Average"
+        default:
+            return "Large"
+        }
+    }
+    
+    var price: String {
+        String(repeating: "$", count: resort.price)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            VStack {
+                Text("Size")
+                    .font(.caption.bold())
+                Text(size)
+                    .font(.title3)
+            }
+            
+            VStack {
+                Text("Price")
+                    .font(.caption.bold())
+                
+                Text(price)
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
-struct ReportsDetailView_Previews: PreviewProvider {
+struct ResortDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ReportsDetailView()
+        ResortDetailView(resort: Resort.example)
     }
 }
