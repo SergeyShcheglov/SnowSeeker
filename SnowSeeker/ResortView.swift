7cd08f261920a84e1c0e28584ad51686b2e74cc8
Sergey Shcheglov <97shcheglov@gmail.com>
2022-03-16 18:03:50 +0600

    Add 2 views in VStack

---
+++ SnowSeeker/ResortView.swift
@@ -0,0 +1,51 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by Sergey Shcheglov on 16.03.2022.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    let resort: Resort
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack {
+                    ResortDetailView(resort: resort)
+                    SkiDetailsView(resort: resort)
+                }
+                .padding()
+                .background(Color.primary.opacity(0.1))
+                
+                Group {
+                    Text(resort.description)
+                        .padding(.vertical)
+                    
+                    Text("Facilities")
+                        .font(.headline)
+                    
+                    Text(resort.facilities, format: .list(type: .and))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationTitle("\(resort.name), \(resort.country)")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ResortView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ResortView(resort: Resort.example)
+        }
+    }
+}
